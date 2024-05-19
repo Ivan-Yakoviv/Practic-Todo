ebf12ec8c7f19a46dc88dcfdc4cf3c05c5da2183
dr-jeky11 <tyrsyna@gmail.com>
2024-05-19 14:41:03 +0200

    li markup added

--- src/main.js
+++ src/main.js
@@ -7,11 +7,28 @@
 // Повинна бути перевірка на порожнє поле
 
 const form = document.querySelector("#task-form");
+const taskList = document.querySelector("#task-list");
+
 form.addEventListener("submit",(event) => {
     event.preventDefault();
     const value = event.target.elements.taskName.value.trim();
     if (value === "") {
 return;
     }
+    generateList(value);
     console.dir(event.target.elements.taskName.value);
 }); 
+
+
+
+//TODO-2
+// Напишіть логіку яка з сабміта буде брати значення поля інпут
+// Генерувати елемент списку LI  з текстом і кнопкою Х, у майбутньому це буде кнопка видалення таски
+
+
+
+function generateList(text) {
+    const markUP = `<li>${text}<button type="button">X</button></li>`
+    taskList.insertAdjacentHTML("beforeend", markUP);
+}
+
