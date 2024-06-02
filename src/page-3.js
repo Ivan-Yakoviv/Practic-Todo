adb310b7278ab28629051c97655d15974825219c
dr-jeky11 <tyrsyna@gmail.com>
2024-06-02 16:02:03 +0200

    page 3 update

--- src/page-3.js
+++ src/page-3.js
@@ -1,11 +1,9 @@
-// import axios from "axios";
-// import { getAllContacts, getElementByID, postElement, deleteContactById, updateContactById } from "./js/apicontacts";
+import axios from "axios";
+import { getAllContacts, getElementByID, postElement, deleteContactById, updateContactById } from "./js/apicontacts";
 
-// getAllContacts();
-// // getElementByID(18);
-// postElement({ name: "Jared", phone: 911 })
-// // deleteContactById(19);
-// updateContactById(20, {name: "Patron", phone: 322})
+getAllContacts();
+// getElementByID(18);
+postElement({ name: "Jared", phone: 911 })
+// deleteContactById(19);
+updateContactById(20, {name: "Patron", phone: 322})
 
-
-console.log(import.meta.env.VITE_BASE_URL);
