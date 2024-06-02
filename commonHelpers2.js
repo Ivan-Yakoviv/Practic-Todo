ad993c75d15648beded1831135c9160fede566e4
dr-jeky11 <153104535+dr-jeky11@users.noreply.github.com>
2024-06-02 12:57:00 +0000

    Deploying to gh-pages from @ Ivan-Yakoviv/Practic-Todo@068c18c435a96a493f0ff761180a0101a22b6374 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{o as a,l as o,a as s,K as e}from"./assets/listedition-75d803dd.js";a(s);o(s,e);
+import{o as e,l as i,a,K as t}from"./assets/listedition-cf3eb501.js";import{S as r,E as s,P as n,N as o}from"./assets/vendor-1ebc4495.js";new r(".swiper",{effect:"flip",grabCursor:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},modules:[s,n,o]});e(a);i(a,t);
 //# sourceMappingURL=commonHelpers2.js.map
