c321c272472f9a505d053adeb6648490087dabf7
dr-jeky11 <153104535+dr-jeky11@users.noreply.github.com>
2024-06-02 14:02:18 +0000

    Deploying to gh-pages from @ Ivan-Yakoviv/Practic-Todo@adb310b7278ab28629051c97655d15974825219c 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";console.log({}.VITE_BASE_URL);
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as c}from"./assets/vendor-9cb839b9.js";const n="https://64859281a795d24810b7146a.mockapi.io/work";async function r(){try{const o=await c.get(`${n}/contacts`)}catch(o){console.log(o)}}async function s(o){try{c.post(`${n}/contacts/`,o)}catch(t){console.log(t)}}async function e(o,t){try{c.put(`${n}/contacts/${o}`,t)}catch(a){console.log(a)}}r();s({name:"Jared",phone:911});e(20,{name:"Patron",phone:322});
 //# sourceMappingURL=commonHelpers3.js.map
